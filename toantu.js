b90f80af9bf798cc791186ddabffebd69776001b
sonta2910 <sontt291099@gmail.com>
2022-11-07 15:42:23 +1100

    toantu

---
+++ toantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("NhapWidth");
+inputHeight = prompt("Nhap Height");
+let Width = parseInt(inputWidth)
+let Height = parseInt(inputHeight)
+let Area = Width * Height
+document.write('The area is: ' + Area)
